a8bec17198acd16e868ce8c60b3ed0d31ef76513
Jakub Grabarski (JAKU0065) <jaku0065@zbc.dk>
2023-03-16 09:30:00 +0100

    Implemented HTTP Post Request
    - Send Post Request with Json body
    - Get Post Response

--- Slave/Slave.ino
+++ Slave/Slave.ino
@@ -1,7 +1,11 @@
 #include <WiFi101.h>
+#include <ArduinoJson.h>
 
 #define ssid "ZBC-E-CH-SKP019 0986"
 #define pass "710%dK14"
+
+#define ip "192.168.137.106"
+#define port 63584
 
 char deviceName[] = "Arduino";
 
@@ -22,7 +26,48 @@ void setup() {
 }
 
 void loop() {
- 
+  if (WiFi.status() == WL_CONNECTED) {
+    WiFiClient http;
+    http.connect(ip, port);
+    if (http.connected()) {
+
+      StaticJsonBuffer<200> jsonBuffer;
+      JsonObject& root = jsonBuffer.createObject();
+      JsonArray& data = root.createNestedArray("Soil Moisture Data");
+      data.add(0);
+      data.add(200);
+      data.add(400);
+      data.add(600);
+      data.add(800);
+      data.add(1000);
+
+      String json;
+      root.printTo(json);
+      //Send HTTP POST request
+      http.println("POST / HTTP/1.1");
+      http.println("Host: " + String(ip));
+      http.println("Content-Type: text/plain");
+      http.println("Content-Length: " + String(json.length()));
+      http.println("Accept-Encoding: gzip, deflate, br");
+      http.println("Accept: */*");
+      http.println("Connection: keep-alive");
+      http.print("\n");
+
+      Serial.println(json);
+      http.println(json);
+      http.println();
+
+      //Read HTTP response from server
+
+      while (http.connected()) {
+        String line = http.readStringUntil('\r');
+        Serial.println(line);
+      }
+
+      http.stop();
+      delay(1000);
+    }
+  }
 }
 
 
